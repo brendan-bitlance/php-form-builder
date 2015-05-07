c8216e9200f659b64a3c01656d0bb17f1841c2dd
Brendan Markham <brendan@bitlance.com.au>
2015-05-07 20:42:58 +0800

    Reduce code for Length validation

--- Validation/Length.php
+++ Validation/Length.php
@@ -20,9 +20,13 @@ class Length extends AbstractValidation
 		$max_exists = !is_null($max);
 		if ($min_exists && !is_int($min)) {
 			throw new \InvalidArgumentException('Min must be empty or integer');
+		} else {
+			$this->min = $min;
 		}
 		if ($max_exists && !is_int($max)) {
 			throw new \InvalidArgumentException('Max must be empty or integer');
+		} else {
+			$this->max = $max;
 		}
 		if (!$min_exists && !$max_exists) {
 			throw new \InvalidArgumentException('Min or max must contain a value');
@@ -30,29 +34,23 @@ class Length extends AbstractValidation
 		if ($min_exists && $max_exists && $min > $max) {
 			throw new \InvalidArgumentException('Min cannot be greater than max');
 		}
-		if ($min_exists) {
-			$this->min = (int) $min;
-		}
-		if ($max_exists) {
-			$this->max = (int) $max;
-		}
 		if (is_null($message)) {
 			$message = "Value must contain ";
 			if ($min === $max) {
-				$message .= "exactly {$this->min}";
+				$message .= "exactly {$min}";
 			} else {
 				if ($min_exists) {
-					$message .= "at least {$this->min}";
+					$message .= "at least {$min}";
 					if ($max_exists) {
 						$message .= " and ";
 					}
 				}
 				if ($max_exists) {
-					$message .= "no more than {$this->max}";
+					$message .= "no more than {$max}";
 				}
 			}
 			$message .= " character";
-			if ($this->min > 1 || $this->max > 1) {
+			if ($min > 1 || $max > 1) {
 				$message.= "s";
 			}
 		}
@@ -61,14 +59,13 @@ class Length extends AbstractValidation
 
 	protected function is_value_valid($value)
 	{
-		$valid = true;
 		$length = strlen($value);
 		if (!is_null($this->min) && $length < $this->min) {
-			$valid = false;
+			return false;
 		}
 		if (!is_null($this->max) && $length > $this->max) {
-			$valid = false;
+			return false;
 		}
-		return $valid;
+		return true;
 	}
 }
